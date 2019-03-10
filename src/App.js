d4222cd5e45b28afeb78d5ca971f3eeee02bc607
Jessica Sanchez <michelle.sr16@gmail.com>
2019-03-09 19:59:07 -0600

    Change my mind: added inputs, onChange text on meme changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import DogeMeme from './doge-meme';
 class App extends Component {
   render() {
     return (
-      <div className="flex col align-items-center">
+      <div className="content">
         <p>Doge Meme</p>
         <DogeMeme 
           topTxt='cuac'
--- src/doge-meme.js
+++ src/doge-meme.js
@@ -2,14 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 
 class DogeMeme extends Component {
-  render() {
-    return (
-      <div className='doge-bg flex col justify-content-between align-items-center'>
-      	<p className="txt">{this.props.topTxt}</p>
-      	<p className="txt">{this.props.bottomTxt}</p>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			topTxt: 'TOP TEXT',
+			bottomTxt: 'BOTTOM TEXT'
+		};
+	}
+
+	handleTextChange(value, prop) {
+		this.setState({
+			[prop]: value.toUpperCase()
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<div className='inputs'>
+					<input placeholder='top text' onChange={(e)=> this.handleTextChange(e.target.value, 'topTxt')}/>
+					<input placeholder='bottom text' onChange={(e)=> this.handleTextChange(e.target.value, 'bottomTxt')}/>
+				</div>
+				<div className='doge-bg flex col justify-content-between align-items-center'>
+				<p className="txt">{this.state.topTxt}</p>
+				<p className="txt">{this.state.bottomTxt}</p>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default DogeMeme;
